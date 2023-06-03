6faf74aed4754c5d8734734c6c0a9292a3be5515
Rakesh Shah <21amtics464@gmail.com>
2023-06-03 22:33:33 +0530

    file_renname

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:flutter/services.dart';
 
-import 'MyVideoPlayer.dart';
+import 'myVideoPlayer.dart';
 
 void main() {
   runApp(const MyApp());
